0dd0a90e62d378cb2eb9eb24f32fe0ec619012bf
prkhr_gupta <prakharofficial30@gmail.com>
2024-07-21 22:30:52 +0530

    review style

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const Listing = require("./models/listing.js");
+const Review = require("./models/review.js");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema, reviewSchema} = require("./schema.js");
 
 const app = express();
 const port = 8080;
@@ -28,9 +29,20 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/WanderList');
 }
 
-//Joi Check
+//Joi Check listing
 const validateListing = (req, res, next) => {
     let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(',');
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+}
+
+//Joi Check review
+const validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
     if(error){
         let errMsg = error.details.map((el) => el.message).join(',');
         throw new ExpressError(400, errMsg);
@@ -53,7 +65,7 @@ app.get("/listings/new", (req, res) => {
 //Show route
 app.get("/listings/:id",  wrapAsync(async (req, res) => {
     let {id} = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", {listing});
 }));
 
@@ -86,6 +98,18 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
+}));
+
+//Review Route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    let {review} = req.body;
+    let newReview = new Review(review);
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+    res.redirect(`/listings/${id}`);
 }));
 
 //Root
--- models/review.js
+++ models/review.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const {Schema} = require("mongoose");
 
 const reviewSchema = new Schema({
-    commrnt: String,
+    comment: String,
     rating: {
         type: Number,
         min: 1,
